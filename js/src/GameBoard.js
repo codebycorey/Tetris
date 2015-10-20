745a705e6a7b92dc253b25c64f998f48a3542ea7
rcodonnell <rcoreyodonnell@gmail.com>
2015-10-19 20:21:11 -0400

    refactor gameboard into own class

---
+++ js/src/GameBoard.js
@@ -0,0 +1,39 @@
+define(["src/Numfont"], function(Numfont) {
+    var GameBoard = Class.extend({
+
+        init: function() {
+            this.back = content.get("back");
+            this.blocks = content.get("blocks");
+            var num = content.get("numbers");
+            this.font = {
+                gray  : new Numfont(num,  0, 9),
+                cyan  : new Numfont(num,  9, 9),
+                red   : new Numfont(num, 18, 9),
+                blue  : new Numfont(num, 27, 9),
+                orange: new Numfont(num, 36, 9),
+                green : new Numfont(num, 45, 9),
+                yellow: new Numfont(num, 54, 9),
+                purple: new Numfont(num, 63, 9)
+            };
+        },
+
+        draw: function(ctx, stat) {
+            var tetris = stat.tetraminos;
+
+            ctx.drawImage(this.back, 0 ,0);
+
+            this.font.orange.draw(ctx, tetris.L, 432, 52, 5);
+            this.font.cyan.draw  (ctx, tetris.I, 432, 76, 5);
+            this.font.purple.draw(ctx, tetris.T, 432, 100, 5);
+            this.font.green.draw (ctx, tetris.S, 432, 124, 5);
+            this.font.red.draw   (ctx, tetris.Z, 432, 148, 5);
+            this.font.yellow.draw(ctx, tetris.O, 432, 172, 5);
+            this.font.blue.draw  (ctx, tetris.J, 432, 196, 5);
+
+            this.font.gray.draw(ctx, tetris.TOT, 425, 221, 6);
+        }
+
+    });
+
+    return GameBoard;
+});
